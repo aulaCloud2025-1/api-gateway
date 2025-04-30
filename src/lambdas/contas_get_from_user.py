c8500d1af0e50a1ca53048a19c4be443a99cdb74
7chii <marinpacper@gmail.com>
2025-04-30 19:38:24 -0300

    update func contas_get_from_user

--- src/lambdas/contas_get_from_user.py
+++ src/lambdas/contas_get_from_user.py
@@ -1,12 +1,20 @@
 import boto3
 import logging
 import json
+from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb')
 table= dynamodb.Table('table_contas')
 
 def get_function(user):
-    table.
+    response = table.query(
+        KeyConditionExpression=Key('User').eq(f'{user}')
+    )
+    answ = {
+        'User':response['Items'][0]['User'],
+        'Mail':response['Items'][0]['Mail']
+    }
+    return answ
 
 def lambda_handler(event, context):
     response = get_function(event['body']['user'])
